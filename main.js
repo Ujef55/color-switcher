a46c9d020b45c85d5c9f65286f9a4b3f7aa8d26c
Ujef55 <ujef.kelawala123@gmail.com>
2023-06-29 10:37:57 +0530

    Added rgb

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const colorButton = document.querySelector('#colorBtn');
 const refreshButton = document.querySelector('#refreshBtn');
+const rgb = document.querySelector('#rgbValue');
 const body = document.querySelector('body');
 
 colorButton.addEventListener('click', function() {
@@ -8,10 +9,11 @@ colorButton.addEventListener('click', function() {
     const b = Math.floor(Math.random() * 255);
     body.style.background = `rgb(${r},${g},${b})`;
     body.style.color = 'white';
+    rgb.textContent = `rgb(${r},${g},${b})`;
 })
 
 refreshButton.addEventListener('click', function() {
     body.style.background = 'white';
     body.style.color = 'black';
-
+    rgb.textContent= '';
 })
